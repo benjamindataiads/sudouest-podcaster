72ebaab7cb957e8348e27e9de81b1a7ec2675e71
Ben <72542719+benicoz@users.noreply.github.com>
2025-12-03 20:52:39 +0100

    Add Sud-Ouest favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Sud-Ouest Podcaster',
   description: 'Générateur automatique de podcasts audio et vidéo basé sur les articles du journal Sud-Ouest',
+  icons: {
+    icon: '/icon.svg',
+  },
 }
 
 export default function RootLayout({
